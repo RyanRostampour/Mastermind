8da7a52552d2db0379721c176d5ee4242f5e550b
Ryan Rostampour <127817513+Dig1talSky@users.noreply.github.com>
2024-07-26 03:01:50 -0400

    Update Mastermind.c

--- Mastermind.c
+++ Mastermind.c
@@ -75,7 +75,6 @@ int main() // Main function.
     {
         printf("Welcome to Mastermind\n"); // Greeting Message.
         printf("Created by: Ryan Rostampour\n"); // Creator Name.
-        printf("110015057\n"); // Student ID.
 
 		printf("Enter the amount of digits:\n"); // Tells a message to the player asking to input amount of guesses.
 		scanf("%d", &LENGTHOFCODE); // Scans the input of the player.
